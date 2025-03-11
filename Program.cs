cfa465c4786654aa9ef9ce89b8c58cf4fb98091f
greengoblin8 <134504798+greengoblin8@users.noreply.github.com>
2025-03-11 09:57:20 +0700

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("kode produk laptop : " + KodeProduk.getKodeProduk("Laptop"));
+        Console.WriteLine("kode produk Smartwatch : " + KodeProduk.getKodeProduk("Smartwatch"));
+        Console.WriteLine("kode produk Kamera : " + KodeProduk.getKodeProduk("Kamera"));
+
+        Console.WriteLine("");
+        Console.WriteLine("");
+
+        FanLaptop fanlaptop = new FanLaptop();
+        fanlaptop.turboShortcut();
+        fanlaptop.modeDown();
+        fanlaptop.modeDown();
+        fanlaptop.modeUp();
+        fanlaptop.modeUp();
+        fanlaptop.turboShortcut();
+    }
+    
+}
